d25b0af0b7fef644bfa258f8f6bea9c130d71da3
GuillaumeBouchetEpitech <guillaume.bouchet@epitech.eu>
2016-10-07 21:21:22 +0100

    now concatenating 3 geometries used as physical trimesh

--- canonjsDemo/app/simulation/circuit.js
+++ canonjsDemo/app/simulation/circuit.js
@@ -22,45 +22,11 @@ define(
 	{
 		//
 		//
-		// duplicate checkpoints
-
-		// function lerp(val1, val2, ratio)
-		// {
-		// 	return val1 + (val2 - val1) * ratio;
-		// }
-
-		// var	tmp_checkpoints = [];
-		// tmp_checkpoints.push(arr_checkpoints[0]);
-		// for (var i = 1; i < arr_checkpoints.length; ++i)
-		// {
-		// 	var l1 = arr_checkpoints[i - 1];
-		// 	var l2 = arr_checkpoints[i];
-
-		// 	for (var f = 0.25; f < 1.0; f += 0.25)
-		// 	{
-		// 		tmp_checkpoints.push([
-		// 			  lerp(l1[0], l2[0], f)
-		// 			, lerp(l1[1], l2[1], f)
-		// 			, lerp(l1[2], l2[2], f)
-		// 			, lerp(l1[3], l2[3], f)
-		// 			, lerp(l1[4], l2[4], f)
-		// 			, lerp(l1[5], l2[5], f)
-		// 		]);
-		// 	}
-
-		// 	tmp_checkpoints.push(l2);
-		// }
-		// arr_checkpoints = tmp_checkpoints;
-
-		//
-		//
 		// polygonise checkpoints
 
 		this._arr_geoms = [];
 
 		var indices = [
-			// 0, 1, 2, // triangle 0
-			// 1, 3, 2, // triangle 1
 			0, 2, 1, // triangle 2
 			1, 2, 3, // triangle 3
 		];
@@ -68,43 +34,9 @@ define(
 		var geom_vertices = [];
 		var geom_vertices_wall = [];
 
-		var prev_normal = null;
 
-		for (var index = 1; index < arr_checkpoints.length; ++index)
+		function calculate_normal(v0, v1, v2)
 		{
-			var c1 = arr_checkpoints[index-1];
-			var c2 = arr_checkpoints[index];
-
-			var p11 = [ c1[0], c1[1], c1[2] ];
-			var p12 = [ c1[3], c1[4], c1[5] ];
-			var p21 = [ c2[0], c2[1], c2[2] ];
-			var p22 = [ c2[3], c2[4], c2[5] ];
-
-			var vertices = [
-			   p11[0], p11[1], p11[2], // vertex 0
-			   p12[0], p12[1], p12[2], // vertex 1
-			   p21[0], p21[1], p21[2], // vertex 2
-			   p22[0], p22[1], p22[2]  // vertex 3
-			];
-			var planeShape = new CANNON.Trimesh(vertices, indices);
-			var planeBody = new CANNON.Body({mass:0});
-			planeBody.addShape(planeShape);
-			planeBody.collisionFilterGroup = -1;
-			planeBody.collisionFilterMask = world._GROUP_sensor; // <- sensors will collide the floor
-			world.addBody(planeBody);
-
-
-
-
-
-			//
-			//
-			// calculate the normal
-
-			var v0 = p12;
-			var v1 = p21;
-			var v2 = p22;
-
 			var px = v1[0] - v0[0];
 			var py = v1[1] - v0[1];
 			var pz = v1[2] - v0[2];
@@ -123,33 +55,95 @@ define(
 			normal[1] /= tmp_len;
 			normal[2] /= tmp_len;
 
-			normal[0] *= 10;
-			normal[1] *= 10;
-			normal[2] *= 10;
+			return normal;
 
-			// calculate the normal
+		} // function calculate_normal(v0, v1, v2)
+
+
+		var prev_normal = null;
+
+		var geometry_step = 3;
+
+		for (var index = 1; index < arr_checkpoints.length; index += geometry_step)
+		{
+			var vertices = [];
+			var vertices_wall1 = [];
+			var vertices_wall2 = [];
+
 			//
-			//
+			// repeatable
 
-
-			if (!prev_normal)
-				prev_normal = normal;
-
-
-
-			//
-			// walls
-
-			var vertices_wall1 = [
-			   // p11[0], p11[1], p11[2],
-			   // p11[0], p11[1], p11[2]+10,
-			   // p21[0], p21[1], p21[2],
-			   // p21[0], p21[1], p21[2]+10
-			   p11[0], p11[1], p11[2],
-			   p11[0]+prev_normal[0], p11[1]+prev_normal[1], p11[2]+prev_normal[2],
-			   p21[0], p21[1], p21[2],
-			   p21[0]+normal[0], p21[1]+normal[1], p21[2]+normal[2]
+			var indices = [
+				0, 2, 1, // triangle 2
+				1, 2, 3, // triangle 3
 			];
+
+			for (var index2 = 1; index2 < geometry_step; ++index2)
+			{
+				var step = index2 * 4;
+				indices.push( indices[0]+step, indices[1]+step, indices[2]+step );
+				indices.push( indices[3]+step, indices[4]+step, indices[5]+step );
+			}
+
+			for (var index2 = index; index2 < arr_checkpoints.length && index2 < index+geometry_step; ++index2)
+			{
+				// console.log(index2);
+				var c1 = arr_checkpoints[index2-1];
+				var c2 = arr_checkpoints[index2];
+
+				var p11 = [ c1[0], c1[1], c1[2] ];
+				var p12 = [ c1[3], c1[4], c1[5] ];
+				var p21 = [ c2[0], c2[1], c2[2] ];
+				var p22 = [ c2[3], c2[4], c2[5] ];
+
+				//
+				// calculate the floor normal
+
+				var normal = calculate_normal(p12, p21, p22);
+
+				normal[0] *= 10;
+				normal[1] *= 10;
+				normal[2] *= 10;
+
+				if (!prev_normal)
+					prev_normal = normal;
+
+				//
+				// floor
+
+				vertices.push( p11[0], p11[1], p11[2] );
+				vertices.push( p12[0], p12[1], p12[2] );
+				vertices.push( p21[0], p21[1], p21[2] );
+				vertices.push( p22[0], p22[1], p22[2] );
+
+				//
+				// walls
+
+				vertices_wall1.push( p11[0], p11[1], p11[2] );
+				vertices_wall1.push( p11[0]+prev_normal[0], p11[1]+prev_normal[1], p11[2]+prev_normal[2] );
+				vertices_wall1.push( p21[0], p21[1], p21[2] );
+				vertices_wall1.push( p21[0]+normal[0], p21[1]+normal[1], p21[2]+normal[2] );
+
+				vertices_wall2.push( p12[0], p12[1], p12[2] );
+				vertices_wall2.push( p12[0]+prev_normal[0], p12[1]+prev_normal[1], p12[2]+prev_normal[2] );
+				vertices_wall2.push( p22[0], p22[1], p22[2] );
+				vertices_wall2.push( p22[0]+normal[0], p22[1]+normal[1], p22[2]+normal[2] );
+
+				prev_normal = normal;
+			}
+
+			// repeatable
+			//
+
+			//
+
+			var planeShape = new CANNON.Trimesh(vertices, indices);
+			var planeBody = new CANNON.Body({mass:0});
+			planeBody.addShape(planeShape);
+			planeBody.collisionFilterGroup = -1;
+			planeBody.collisionFilterMask = world._GROUP_sensor; // <- sensors will collide the floor
+			world.addBody(planeBody);
+
 			var planeShape = new CANNON.Trimesh(vertices_wall1, indices);
 			var planeBody = new CANNON.Body({mass:0});
 			planeBody.addShape(planeShape);
@@ -157,16 +151,6 @@ define(
 			planeBody.collisionFilterMask = world._GROUP_sensor;
 			world.addBody(planeBody);
 
-			var vertices_wall2 = [
-			   // p12[0], p12[1], p12[2],
-			   // p12[0], p12[1], p12[2]+10,
-			   // p22[0], p22[1], p22[2],
-			   // p22[0], p22[1], p22[2]+10
-			   p12[0], p12[1], p12[2],
-			   p12[0]+prev_normal[0], p12[1]+prev_normal[1], p12[2]+prev_normal[2],
-			   p22[0], p22[1], p22[2],
-			   p22[0]+normal[0], p22[1]+normal[1], p22[2]+normal[2]
-			];
 			var planeShape = new CANNON.Trimesh(vertices_wall2, indices);
 			var planeBody = new CANNON.Body({mass:0});
 			planeBody.addShape(planeShape);
@@ -174,11 +158,8 @@ define(
 			planeBody.collisionFilterMask = world._GROUP_sensor;
 			world.addBody(planeBody);
 
-			prev_normal = normal;
-
 			//
 			//
-
 
 			for (var index2 in indices)
 			{
@@ -203,7 +184,8 @@ define(
 
 				geom_vertices_wall.push( vertices_wall2[vertexi+0], vertices_wall2[vertexi+1], vertices_wall2[vertexi+2], 0,0.5,0.5 );
 			}
-		}
+
+		} // for (var index = 1; index < arr_checkpoints.length; index += 2)
 
 		var geom_cube2 = new createGeometryColor(geom_vertices, gl.TRIANGLES);
 		this._arr_geoms.push(geom_cube2);
