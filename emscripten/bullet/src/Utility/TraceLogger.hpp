aaec8cfd926b534b1357c5e9ddbe1f7bc65310d0
GuillaumeBouchetEpitech <guillaume.bouchet@epitech.eu>
2017-11-21 20:34:10 +0000

    TraceLogger => improved

--- emscripten/bullet/src/Utility/TraceLogger.hpp
+++ emscripten/bullet/src/Utility/TraceLogger.hpp
@@ -1,15 +1,10 @@
 
 
-
-#ifndef D_TRACELOGGER_HPP
-#define D_TRACELOGGER_HPP
-
+#pragma once
 
 
 #include <sstream>
 #include <cstring> // -> __FILENAME__
-
-
 
 
 #include <iostream>
@@ -21,23 +16,20 @@ std::string getTime();
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define D_STACK "(" << __FILENAME__ << "|" << __func__ << "|" << __LINE__ << ")"
 
-// #define D_MYLOG(msg)\
-// {\
-// 	std::stringstream sstr;\
-// 	sstr << "MYLOG [" << getTime() << "] " << D_STACK << " -> " << msg;\
-// 	std::cout << sstr.str() << std::endl;\
-// }
-
 #define D_MYLOG(msg)\
 {\
 	std::stringstream sstr;\
-	sstr << "MYLOG [" << getTime() << "] -> " << msg;\
+	sstr << "MYLOG [" << getTime() << "] " << D_STACK << " -> " << msg;\
 	std::cout << sstr.str() << std::endl;\
 }
 
+// #define D_MYLOG(msg)\
+// {\
+// 	std::stringstream sstr;\
+// 	sstr << "MYLOG [" << getTime() << "] -> " << msg;\
+// 	std::cout << sstr.str() << std::endl;\
+// }
+
 // #define D_MYLOG(msg)
-
-
-#endif // D_TRACELOGGER_HPP
 
 
